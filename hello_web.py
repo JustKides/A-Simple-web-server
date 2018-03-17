fa9e6234a910c3ab25caec56a47ed59997f54417
JustKides <goaheadhong@outlook.com>
2018-03-17 11:49:14 +0800

    Logg
    Logg

--- hello_web.py
+++ hello_web.py
@@ -1,6 +1,65 @@
 import BaseHTTPServer
 import os
+
+#----------------------------------------------------------------------            
+class case_no_file(object):
+    '''File or directory does not exist.'''
+
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+
+    def act(self, handler):
+        raise ServerException("'{0}' not found".format(handler.path))
+
+#----------------------------------------------------------------------
+class case_existing_file(object):
+    '''File exists.'''
+
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+
+    def act(self, handler):
+        handler.handle_file(handler.full_path)
+
+#----------------------------------------------------------------------
+class case_always_fail(object):
+    '''Base case if nothing else worked.'''
+
+    def test(self, handler):
+        return True
+
+    def act(self, handler):
+        raise ServerException("Unknown object '{0}'".format(handler.path))
+#----------------------------------------------------------------------
+class case_directory_index_file(object):
+    '''Serve index.html page for a directory.'''
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+               os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        handler.list_dir(handler.full_path)            
+
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):    
+
+    Cases = [case_no_file(),
+             case_existing_file(),
+             case_directory_index_file(),
+             case_always_fail()]
+
+    Listing_Page = '''\
+                    <html>
+                    <body>
+                    <ul>
+                    {0}
+                    </ul>
+                    </body>
+                    </html>
+                   '''
 
     Error_Page = """\
         <html>
@@ -13,14 +72,18 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     # handling get method
     def do_GET(self):
         try:
-            full_path = os.getcwd() + self.path            
 
-            if not os.path.exists(full_path):                
-                raise ServerException("'{0}' not found".format(self.path))
-            elif os.path.isfile(full_path):
-                self.handle_file(full_path)
-            else:            
-                raise ServerException("Unknown object '{0}'".format(self.path))
+            # Figure out what exactly is being requested.
+            self.full_path = os.getcwd() + self.path
+
+            # Figure out how to handle it.
+            for case in self.Cases:
+                handler = case()
+                if handler.test(self):
+                    handler.act(self)
+                    break
+
+        # Handle errors.
         except Exception as msg:
             self.handle_error(msg)
 
@@ -43,6 +106,18 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_header("Content-Length", str(len(content)))
         self.end_headers()
         self.wfile.write(content)
+
+    def list_dir(self, full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) 
+                for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(page)
+        except OSError as msg:
+            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
+            self.handle_error(msg)        
+
 #----------------------------------------------------------------------
 
 if __name__ == '__main__':
